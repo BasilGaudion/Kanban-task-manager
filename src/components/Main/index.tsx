1a53d6f80a347678bb7659af6134a6df71a7f175
BasilGaudion <basil.gaudion@yahoo.fr>
2023-08-02 19:44:05 +0200

    feature/add column slider

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/scss';
+import 'swiper/scss/pagination';
+
 import './styles.scss';
 import Column from '../Column';
+
+import { Pagination } from 'swiper/modules';
 
 const Main = () => {
   const [haveColums, setHaveColums] = useState(true);
@@ -10,10 +18,23 @@ const Main = () => {
         {haveColums 
         
           ? 
+          
           <div className='main__container container'>
-            <Column/>
+            <Swiper
+              slidesPerView={'auto'}
+              // spaceBetween={35}
+              className="mySwiper"
+            >
+              <SwiperSlide><Column/></SwiperSlide>
+              <SwiperSlide><Column/></SwiperSlide>
+              <SwiperSlide><Column/></SwiperSlide>
+              <SwiperSlide><Column/></SwiperSlide>
+            </Swiper>
+            
           </div>
+
           : 
+
           <div className='main__container--empty container'>
             <h3 className='main__empty-board'>This board is empty. Create a new column to get started.</h3>
             <button type='button' className='main__button-column'>+ Add New Column</button>
