6c1c8eb66a8ae8a8ea24dccc1172507a63af2492
BasilGaudion <basil.gaudion@yahoo.fr>
2023-08-02 15:08:52 +0200

    feature/static mobile header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import './styles.scss';
-import { LogoMobile } from '../../assets';
+import { LogoMobile, IconChevronDown, IconCross, IconVerticalEllipsis } from '../../assets';
 const Header = () => {
 
   return (
     <header className='header'>
-      <div className='header__container'>
+      <div className='header__container container'>
         <div className='header__left-group'>
-          <img src={LogoMobile} alt="Logo Mobile" />
+          <img src={LogoMobile} className="header__logo" alt="Logo Mobile" />
           <h2 className='header__board-name'>Platform Launch</h2>
-          <svg className='header__chevron-down' width="10" height="7" xmlns="http://www.w3.org/2000/svg"><path stroke="#635FC7" stroke-width="2" fill="none" d="m1 1 4 4 4-4"/></svg>
+          <img src={IconChevronDown} alt="" />
         </div>
         <div className='header__right-group'>
-          <button type='button'>
-            test
+          <button type='button' className='header__add-task'>
+            +
           </button>
+          <img src={IconVerticalEllipsis} className='header__ellipsis'alt="" />
         </div>
 
       </div>
